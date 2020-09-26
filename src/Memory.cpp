9eddb0dfd6882ff45674843d05e31d7ad75e1236
ThorstenB <brehmt@gmail.com>
2020-09-26 14:41:16 +0200

    Fixed AltZP access / completed partial revert from earlier commit See commit 0ce4b71334966da2285967cc9215a1a3a14b0395 or issue #96. The AltZP code was only partially reverted with the commit above. Accidentally there is still an "#if 0...#else..." activating the broken code for the D0.. address range. The revert only accidentally only affected the E0.. address range. Games like GATO or Karteka did no longer load (since their loading routine was using a zero page pointer to write data to the affected address range).

--- src/Memory.cpp
+++ src/Memory.cpp
@@ -678,7 +678,7 @@ void MemUpdatePaging(BOOL initialize, BOOL updatewriteonly) {
 
   for (loop = 0xD0; loop < 0xE0; loop++) {
     int bankoffset = (SW_HRAM_BANK2 ? 0 : 0x1000);
-#if 0
+#if 1
 		memshadow[loop] = SW_HIGHRAM
 												? SW_ALTZP
 													?	memaux + (loop << 8) - bankoffset
