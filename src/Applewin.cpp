ae355ca250f39dbc933c997ae20532101ff35b33
LEE Sau Dan <leesaudan@gmail.com>
2021-01-08 10:44:18 +0800

    bug fix: uninitialized variable was accessed

--- src/Applewin.cpp
+++ src/Applewin.cpp
@@ -433,7 +433,7 @@ void setAutoBoot()
 void LoadConfiguration()
 {
   if (registry) {
-    DWORD dwComputerType;
+    DWORD dwComputerType = g_Apple2Type;
     LOAD(TEXT("Computer Emulation"), &dwComputerType);
 
     switch (dwComputerType) {
